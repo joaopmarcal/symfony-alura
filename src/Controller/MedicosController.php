0bdedba0fda522102255c958dcceffd35767c91d
João Paulo <joaopaulozangrande@gmail.com>
2021-10-04 18:54:54 -0300

    I installed and I configured the ORM Doctrine

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -7,9 +7,17 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MedicosController
 {
+
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
 
     /**
      * @Route("/medicos", methods={"POST"})
@@ -22,6 +30,9 @@ class MedicosController
         $medico = new Medico();
         $medico->crm = $dadoEmJson->crm;
         $medico->nome = $dadoEmJson->nome;
+
+        $this->entityManager->persist($medico);
+        $this->entityManager->flush();
 
         return new JsonResponse($medico);
     }
--- src/Entity/Medico.php
+++ src/Entity/Medico.php
@@ -2,6 +2,12 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+
 class Medico
 {
     public $crm;
