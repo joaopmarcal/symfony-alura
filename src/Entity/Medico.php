8bf8413584a981a64ca25d9b67f17ce2ac2731f2
João Paulo <joaopaulozangrande@gmail.com>
2021-10-07 21:43:45 -0300

    I corrected the medico register

--- src/Entity/Medico.php
+++ src/Entity/Medico.php
@@ -8,28 +8,57 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity()
  */
 
-class Medico
+class Medico implements \JsonSerializable
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    public $id;
+    private $id;
     /**
      * @ORM\Column(type="integer")
      */
-    public $crm;
+    private $crm;
     /**
      * @ORM\Column(type="string")
      */
-    public $nome;
+    private $nome;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\especialidade")
      * @ORM\JoinColumn(nullable=false)
      */
     private $especialidade;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCrm(): ?int
+    {
+        return $this->crm;
+    }
+
+    public function setCrm(int $crm): self
+    {
+        $this->crm = $crm;
+        return $this;
+
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+        return $this;
+
+    }
 
     public function getEspecialidade(): ?especialidade
     {
@@ -41,5 +70,15 @@ class Medico
         $this->especialidade = $especialidade;
 
         return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->getId(),
+            'crm' => $this->getCrm(),
+            'nome' => $this->getNome(),
+            'especialidadeId' => $this->getEspecialidade()->getId(),
+        ];
     }
 }
--- src/Helper/MedicoFactory.php
+++ src/Helper/MedicoFactory.php
@@ -3,16 +3,28 @@
 namespace App\Helper;
 
 use App\Entity\Medico;
+use App\Repository\EspecialidadeRepository;
 
 class MedicoFactory
 {
+    private $especialidadeRepository;
+
+    public function __construct(EspecialidadeRepository $especialidadeRepository)
+    {
+        $this->especialidadeRepository = $especialidadeRepository;
+    }
+
     public function criarMedico(string $json): Medico
     {
         $dadoEmJson = json_decode($json);
+        $especialidadeId = $dadoEmJson->especialidadeId;
+        $especialidade = $this->especialidadeRepository->find($especialidadeId);
 
         $medico = new Medico();
-        $medico->crm = $dadoEmJson->crm;
-        $medico->nome = $dadoEmJson->nome;
+        $medico
+            ->setCrm($dadoEmJson->crm)
+            ->setNome($dadoEmJson->nome)
+            ->setEspecialidade($especialidade);
 
         return $medico;
     }
